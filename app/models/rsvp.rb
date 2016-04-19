c24932c789d7ce45a740599618f5337b24195ebb
cmkoller <cmkoller01@gmail.com>
2016-04-18 22:18:19 -0400

    Update RSVP form

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -1,5 +1,5 @@
 class RSVP < ActiveRecord::Base
   validates :name, presence: true
   validates :attending_ceremony, presence: true
-  validates :attending_friday, presence: true
+  validates :attending_friday, inclusion: { in: [true, false] }
 end
